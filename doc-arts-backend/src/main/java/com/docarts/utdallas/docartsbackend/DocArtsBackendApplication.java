9ba91907c615b7fe9a30c51d6f1684f9839bf08f
nandinithota-1 <nandinithota2014@gmail.com>
2024-10-15 00:21:11 -0500

    init

--- doc-arts-backend/src/main/java/com/docarts/utdallas/docartsbackend/DocArtsBackendApplication.java
+++ doc-arts-backend/src/main/java/com/docarts/utdallas/docartsbackend/DocArtsBackendApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DocArtsBackendApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DocArtsBackendApplication.class, args);
 	}
