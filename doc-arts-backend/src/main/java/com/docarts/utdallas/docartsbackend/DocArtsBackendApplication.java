448e13a2bc628ea01f51c8b76caee8a69e2fc404
nandinithota-1 <nandinithota2014@gmail.com>
2024-10-14 23:36:51 -0500

    init

---
+++ doc-arts-backend/src/main/java/com/docarts/utdallas/docartsbackend/DocArtsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.docarts.utdallas.docartsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocArtsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocArtsBackendApplication.class, args);
+	}
+
+}
---
+++ doc-arts-backend/src/test/java/com/docarts/utdallas/docartsbackend/DocArtsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docarts.utdallas.docartsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocArtsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
